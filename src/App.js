1559d84adafc5e13fa7df1d3da5e3f8a069b5ab4
ShijuPJohn <shijupjohn4u@gmail.com>
2021-05-16 23:36:15 +0530

    initial commit. clean up boilerplate

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React, {Component} from "react";
+
+class App extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div>
+                    <h1>Hello</h1>
+                </div>
+            </React.Fragment>
+        );
+    }
+
+}
+
+export default App;
